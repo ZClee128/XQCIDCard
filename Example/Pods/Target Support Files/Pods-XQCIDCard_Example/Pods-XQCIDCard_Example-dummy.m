f212a0559a6451a9185bd2ea856c8ef23c657d34
lee <876231865@qq.com>
2019-08-19 09:34:15 +0800

    初始化项目

---
+++ Example/Pods/Target Support Files/Pods-XQCIDCard_Example/Pods-XQCIDCard_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XQCIDCard_Example : NSObject
+@end
+@implementation PodsDummy_Pods_XQCIDCard_Example
+@end
---
+++ Example/Pods/Target Support Files/XQCIDCard/XQCIDCard-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XQCIDCard : NSObject
+@end
+@implementation PodsDummy_XQCIDCard
+@end
